0144c209bacce2b0a7e3c1f38c955d4254f67ef2
septyo adi <beelzebub5758@gmail.com>
2024-10-21 22:29:52 +0700

    tampilan home

---
+++ app/src/main/java/com/example/umn_kantin/TenantProfile.kt
@@ -0,0 +1,17 @@
+package com.example.umn_kantin
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.content.Intent
+
+class TenantProfile : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tenant_profile) // Mengatur layout ke tenant_profile.xml
+
+        
+
+    }
+}
